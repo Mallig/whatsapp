614b2956fa86eb6ea0a8062d6ae30d60d41b2471
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-10-05 18:11:03 +0100

    Initial commit - Initializes app and adds first component

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SignUpForm extends React.Component {
+  
+  render() {
+    return (<div><h1>hello you want to sign up</h1></div>)
+  }
+
+}
+
+export default SignUpForm;
