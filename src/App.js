98d55b838d7bcbfce54b760421280f0bb6f68654
Hugo Sykes <hugo@denmeadfarm.co.uk>
2018-10-06 18:01:43 +0100

    Fleshes out sign up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import SignUpForm from './components/SignUpForm'
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      loggedIn: false
+    }
+  }
+
+  onSubmit = (data) => {
+    this.setState({loggedIn: true, formData: data, formDetails: "email: " + data.email})
+  }
+  
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <SignUpForm/>
+          {(!this.state.loggedIn
+            ? <SignUpForm onSubmit={this.onSubmit}/>
+            : this.state.formDetails
+          )}
         </header>
       </div>
     );
   }
+
+
+
+
+
 }
 
 export default App;
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,18 +1,29 @@
 import React from 'react';
 
 class SignUpForm extends React.Component {
+
+  dealWithFormData = (email, password, confirmPassword) => {
+    return {email: email.value, password: password.value, confirmPassword: confirmPassword.value}
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.data = this.dealWithFormData(...e.target)
+    console.log(this.data)
+    this.props.onSubmit(this.data)
+  }
   
   render() {
     return (
     <div>
       <h1>hello you want to sign up</h1>
-      <form action="/register" method="post">
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <label htmlFor='email'>Enter your email address:</label>
-        <input name='email' id='email' type='email' required/><br/>
+        <input name='email' id='email' type='text' required/><br/>
         <label htmlFor='password'>Enter your password:</label>
         <input name='password' id='password' type='password' required/><br/>
         <label htmlFor='confirm-password'>Confirm your password:</label>
-        <input name='confirm-password' id='confirm-password' type='password' required/><br/>
+        <input name='confirmPassword' id='confirmPassword' type='password' required/><br/>
         <button type='submit'>Submit</button>
       </form>
     </div>)
