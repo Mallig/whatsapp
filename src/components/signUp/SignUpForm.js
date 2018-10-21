4a5ef7f30198f7bb776ccffbcd6d0c68e093eeda
Hugo Sykes <hugo@denmeadfarm.co.uk>
2018-10-21 17:54:01 +0100

    Rearranges signUpForm html

--- src/components/signUp/SignUpForm.js
+++ src/components/signUp/SignUpForm.js
@@ -40,12 +40,18 @@ class SignUpForm extends React.Component {
       <div id='signUpForm'>
         <h1>hello you want to sign up</h1>
         <form onSubmit={this.handleSubmit.bind(this)}>
-          <label htmlFor='username'>Enter your username :</label>
-          <input name='username' id='username' type='text' required/><br/>
-          <label htmlFor='password'>Enter your password:</label>
-          <input name='password' id='password' type='password' required/><br/>
-          <label htmlFor='confirm-password'>Confirm your password:</label>
-          <input name='confirmPassword' id='confirmPassword' type='password' required/><br/>
+          <div id='signUp-username'>
+            <label htmlFor='username'>Enter your username :</label>
+            <input name='username' id='username' type='text' required/><br/>
+          </div>
+          <div id='signUp-password'>
+            <label htmlFor='password'>Enter your password:</label>
+            <input name='password' id='password' type='password' required/><br/>
+          </div>
+          <div id='signUp-confirmPassword'>
+            <label htmlFor='confirm-password'>Confirm your password:</label>
+            <input name='confirmPassword' id='confirmPassword' type='password' required/><br/>
+          </div>
           <button type='submit'>submit that s**t</button>
         </form>
         <InvalidDetailsError invalidDetails={!this.state.data.valid}/>
