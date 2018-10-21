dc8c519c34cfe02248ceedf70c8bb7d5327b5576
Hugo Sykes <hugo@denmeadfarm.co.uk>
2018-10-21 11:18:22 +0100

    Refactors MainScreen to be function not class as it possesses no state

--- src/components/main/MainScreen.js
+++ src/components/main/MainScreen.js
@@ -2,17 +2,12 @@ import React from 'react'
 import Navbar from './Navbar'
 import Messages from './messages/Messages'
 
-class MainScreen extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <div>This is the main screen. Welcome to Mal and Hugo's Whatsapp clone, {this.props.username}</div>
-                <Navbar orientation='left'/><Navbar orientation='right'/>
-                <Messages/>
-            </div>
-        )
-    }
-}
+const MainScreen = (props) => (
+    <div>
+        <div>This is the main screen. Welcome to Mal and Hugo's Whatsapp clone, {props.username}</div>
+        <Navbar orientation='left'/><Navbar orientation='right'/>
+        <Messages/>
+    </div>
+)
 
 export default MainScreen
