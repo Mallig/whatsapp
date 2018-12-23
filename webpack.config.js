77ec195d0bc9b647bd2abf00e0ca48ff126f1108
Hugo Sykes <hugo@denmeadfarm.co.uk>
2018-12-23 17:37:50 +0000

    Adds babel and webpack for packaging and transpiling the code

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['css-loader']
+      }
+    ]
+  }
+};
