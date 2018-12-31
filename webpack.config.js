76d1074279421ee11a08045abccf366ea48a50d8
Hugo Sykes <hugo@denmeadfarm.co.uk>
2018-12-31 16:33:36 +0000

    Adds uglifying plugin but not implemented it yet

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 
 module.exports = {
@@ -20,5 +21,8 @@ module.exports = {
         use: ['css-loader']
       }
     ]
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
   }
 };
