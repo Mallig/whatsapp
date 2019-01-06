97b63a49070e917d38f6934a47962eba5ad6a742
Hugo Sykes <hugo@denmeadfarm.co.uk>
2019-01-06 12:33:47 +0000

    Adds jsonwebtoken library

--- src/components/main/UsersClient.js
+++ src/components/main/UsersClient.js
@@ -15,7 +15,7 @@ export class UsersClient {
             return response
         } catch (error) {
             console.error(error)
-            return null
+            return error
         }
     }
 }
