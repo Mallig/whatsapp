df98fbe06c0cf96bf1a017312274791f60c9ad43
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-01-27 21:01:00 +0000

    Now doesn't blow up if no data present

--- src/components/main/centerThing/messages/MessageList.js
+++ src/components/main/centerThing/messages/MessageList.js
@@ -5,11 +5,9 @@ import ListWrapper from '../../ListWrapper'
 
 const MessageList = ({ messages }) => {
     return <ListWrapper inputHeight="60vh">
-        {(messages !== undefined) ?
-            messages.map(message => {
-                return <Message key={message.id} content={message.content} sender_id={message.sender_id}/>
-            }) : null
-        }
+        {messages.map(message => {
+            return <Message key={message.id} content={message.content} sender_id={message.sender_id}/>
+        })}
     </ListWrapper>
 }
 
